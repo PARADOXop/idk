e706f48148ddeba200e18913cf67988396f3b8e2
PARADOXop <ravirajkukade11@gmail.com>
2022-10-12 18:27:35 +0530

    'added a flask file'

--- project.py
+++ project.py
@@ -1,1 +1,16 @@
 print('hello world')
+
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['POST', 'GET'])
+
+def Index():
+    return 'CI/CD pipeline has been created'
+
+
+if __name__ == '__main__':
+    app.run()
